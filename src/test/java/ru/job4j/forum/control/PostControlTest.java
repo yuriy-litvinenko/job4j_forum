75b435044bb4781bb07a5534fc4512753474d6ab
Yuriy Litvinenko <jurij.litvinenko@gmail.com>
2022-09-09 19:59:19 +0600

    1. Spring mock [#2096 #341517]

--- src/test/java/ru/job4j/forum/control/PostControlTest.java
+++ src/test/java/ru/job4j/forum/control/PostControlTest.java
@@ -1,16 +1,24 @@
 package ru.job4j.forum.control;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.Post;
+import ru.job4j.forum.service.PostService;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -23,8 +31,42 @@ class PostControlTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private PostService postService;
+
+    @Captor
+    private ArgumentCaptor<Post> argument;
+
     @Test
     @Order(1)
+    @WithMockUser
+    public void shouldReturnSavePost() throws Exception {
+        this.mockMvc.perform(post("/save")
+                        .param("name", "Куплю ладу-гранта. Дорого."))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection());
+        Mockito.verify(postService).save(argument.capture());
+        MatcherAssert.assertThat(argument.getValue().getName(), is("Куплю ладу-гранта. Дорого."));
+    }
+
+    @Test
+    @Order(2)
+    @WithMockUser
+    public void shouldSaveThenEditThenReturnUpdatePost() throws Exception {
+        this.mockMvc.perform(get("/edit/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("edit"));
+        this.mockMvc.perform(post("/save")
+                        .param("name", "Куплю ауди."))
+                .andDo(print())
+                .andExpect(status().isFound());
+        Mockito.verify(postService).save(argument.capture());
+        MatcherAssert.assertThat(argument.getValue().getName(), is("Куплю ауди."));
+    }
+
+    @Test
+    @Order(3)
     @WithMockUser
     public void shouldReturnDefaultMessageAddPost() throws Exception {
         this.mockMvc.perform(get("/addPost"))
@@ -34,7 +76,7 @@ class PostControlTest {
     }
 
     @Test
-    @Order(2)
+    @Order(4)
     @WithMockUser
     public void shouldReturnDefaultMessageSavePost() throws Exception {
         this.mockMvc.perform(post("/save"))
@@ -44,7 +86,7 @@ class PostControlTest {
     }
 
     @Test
-    @Order(3)
+    @Order(5)
     @WithMockUser
     public void shouldReturnDefaultMessageGetPost() throws Exception {
         this.mockMvc.perform(get("/post/1"))
@@ -54,7 +96,7 @@ class PostControlTest {
     }
 
     @Test
-    @Order(4)
+    @Order(6)
     @WithMockUser
     public void shouldReturnDefaultMessageEditPost() throws Exception {
         this.mockMvc.perform(get("/edit/1"))
--- src/test/java/ru/job4j/forum/control/RegControlTest.java
+++ src/test/java/ru/job4j/forum/control/RegControlTest.java
@@ -1,14 +1,23 @@
 package ru.job4j.forum.control;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.job4j.forum.Main;
+import ru.job4j.forum.model.User;
+import ru.job4j.forum.service.UserServiceRep;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -18,6 +27,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class RegControlTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserServiceRep userServiceRep;
+
+    @Captor
+    private ArgumentCaptor<User> argument;
+
+    @Test
+    @WithMockUser
+    public void shouldReturnRegUser() throws Exception {
+        this.mockMvc.perform(post("/reg")
+                        .param("username", "admin")
+                        .param("password", "123456"))
+                .andDo(print())
+                .andExpect(status().isOk());
+        Mockito.verify(userServiceRep).create(argument.capture());
+        MatcherAssert.assertThat(argument.getValue().getUsername(), is("admin"));
+    }
 
     @Test
     @WithMockUser
